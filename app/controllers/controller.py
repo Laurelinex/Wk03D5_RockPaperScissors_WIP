3ab49bb88d5a546fff1298becff90c79ea0c84d2
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-24 10:54:44 +0000

    added rules and tips

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -7,16 +7,24 @@ from app.models.game import Game
 def index():
     return render_template('index.html', title='RPS')
 
-# @app.route('/play')
-# def start():
-#     return render_template('play.html')
+@app.route('/play')
+def play():
+    return render_template('play.html', title='Play')
 
-@app.route('/<player_1_choice>/<player_2_choice>')
-def play(player_1_choice, player_2_choice):
+@app.route('/rules')
+def display_rules():
+    return render_template('rules.html', title='Rules')
+
+@app.route('/tips')
+def display_tips():
+    return render_template('tips.html', title='Tips')
+
+@app.route('/play/<player_1_choice>/<player_2_choice>')
+def start(player_1_choice, player_2_choice):
     player_1 = Player("Player 1", player_1_choice)
     player_2 = Player("Player 2", player_2_choice)
     game = Game()
     result = game.play_game(player_1, player_2)
     # Result is equal to calling the result of function play_game() declared in the Game class.
     print(result)
-    return render_template('index.html', title='RPS', player1=player_1, player2=player_2, result=result)
+    return render_template('result.html', title='RPS', player1=player_1, player2=player_2, result=result)
